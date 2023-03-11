e6681f7b889da3cd1a216b47b95761ca20112e41
simonweigold <simonw750@gmail.com>
2023-03-11 20:45:09 +0100

    introduced new section for data cleaning

--- scripts/01-import.R
+++ scripts/01-import.R
@@ -3,3 +3,12 @@ library("rio")
 library("here")
 
 ERD <- haven::read_dta(here::here("data", "ERD e 2020 no labels.dta"))
+
+#clean variables of unemployment (v703e in; v705e out) for defined borders according to codebook
+ERD <- ERD %>% 
+  filter(v703e >= 0 & v703e <= 22.2) %>%
+  filter(v705e >= 0.1 & v705e <= 22.2)
+
+#remove missing values
+ERD <- ERD %>%
+  filter(v415e != 88888)
